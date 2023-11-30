657ac0c0888a597b42bd3f3b646e74d4789c5bc8
Всеволод Сорокин <v.kutuzov@wellll.ru>
2023-11-30 16:52:14 +0300

    Android HSE lab2

---
+++ app/src/main/java/hse/course/android_hse_course_lab2/MainActivity.java
@@ -0,0 +1,53 @@
+package hse.course.android_hse_course_lab2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import hse.course.android_hse_course_lab2.adapter.ProductAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProductAdapter productAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        setContentView(R.layout.main_activity_layout);
+
+        RecyclerView recyclerView = findViewById(R.id.productList);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        productAdapter = new ProductAdapter(new ArrayList<>());
+        recyclerView.setAdapter(productAdapter);
+    }
+
+    public void save(View view) {
+        EditText productNameEditText = findViewById(R.id.productName);
+        String productName = productNameEditText.getText().toString();
+        productNameEditText.setText("");
+
+        EditText productNumberEditText = findViewById(R.id.productNumber);
+        String productNumber = productNumberEditText.getText().toString();
+        productNumberEditText.setText("");
+
+        productAdapter.save(productName, productNumber);
+    }
+
+    public void clear(View view) {
+        productAdapter.clearAll();
+    }
+}
